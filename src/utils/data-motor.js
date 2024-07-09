7ebf663fd426fed1c29cd37017258fe20887c712
rizkidf <rizkidwi1418@gmail.com>
2024-07-09 17:46:02 +0700

    post image

--- src/utils/data-motor.js
+++ src/utils/data-motor.js
@@ -1,5 +1,11 @@
-import honda1 from "../assets/Rectangle 5.png"
-import honda2 from "../assets/Rectangle 6.png"
+import honda1 from "../assets/cbr150r.png"
+import honda2 from "../assets/cbr250rr.png"
+import honda3 from "../assets/cb150r.png"
+import yamaha1 from "../assets/r15v3.png"
+import yamaha2 from "../assets/r25v3.png"
+import yamaha3 from "../assets/VixionR155.png"
+import kawasaki1 from "../assets/NinjaSS150.png"
+import kawasaki2 from "../assets/zx250r.png"
 
 export const dataMotor = [
     {
@@ -20,34 +26,51 @@ export const dataMotor = [
     },
     {
         id: 3,
+        name: "Honda CB150R",
+        category: "honda",
+        tahun: "2024",
+        warna: "BLack, Red",
+        image: honda3,
+    },
+    {
+        id: 4,
         name: "Yamaha R15",
         category: "yamaha",
         tahun: "2023",
         warna: "Deep Purple, Blue Metalic",
-        image: honda1,
+        image: yamaha1,
     },
     {
-        id: 4,
+        id: 5,
         name: "Yamaha R25",
         category: "yamaha",
         tahun: "2021",
         warna: "Deep Purple, Blue Metalic",
-        image: honda2,
+        image: yamaha2,
     },
     {
-        id: 5,
+        id: 6,
+        name: "Yamaha VixionR155",
+        category: "yamaha",
+        tahun: "2024",
+        warna: "Red, Blue Metalic",
+        image: yamaha3,   
+
+    },
+    {
+        id: 7,
         name: "Kawasaki Ninja 150ss",
         category: "kawasaki",
         tahun: "2013",
         warna: "Red, Blue, Yellow, Green",
-        image: honda1,
+        image: kawasaki1,
     },
     {
-        id: 6,
+        id: 8,
         name: "Kawasaki ZX25R",
         category: "kawasaki",
         tahun: "2021",
         warna: "Blue, Red, Green",
-        image: honda2,
+        image: kawasaki2,
     }
 ]
